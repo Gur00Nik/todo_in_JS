65abdc89824d48e9d860cdf71acd776435889626
nikita <ngurlov@gmail.com>
2024-05-16 17:08:25 +0300

    use global object Tasks

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const buttonDeleteSpecific = document.querySelector(".delete");
 const buttonInput = containerInput.lastElementChild;
 const input = containerInput.firstElementChild;
 
+let tasks = [];
+
+let selectedTasks = [];
+
 const getLastCharInInput = () => {
   let valueInput = input.value;
   firstCharInInput.textContent = `Ваш последний введенный символ - ${
@@ -15,49 +19,113 @@ const getLastCharInInput = () => {
   Введенный текст - ${valueInput}`;
 };
 
+const renderToDoList = () => {
+  todoList.innerHTML = `${tasks
+    .map(
+      ({ text, id }, index) => `<div data-id="${id}" class="todo__item">
+     <div class="todo__text">${index}. ${text}</div>
+        <button class="button__delete">Удалить</button>
+        <input class="input__checkbox"type="checkbox" value="checked"> 
+     </div>`
+    )
+    .join("")}`;
+};
+
 const addToDo = () => {
-  if (input.value.trim() == "") {
+  if (!input.value.trim()) {
     alert("Введите задачу");
+    return {
+      shouldRerender: false,
+    };
   } else {
+    tasks.push({
+      id: Date.now(),
+      text: input.value,
+    });
     firstCharInInput.textContent = "";
-
-    todoList.innerHTML += ` <div class="todo__item">
-      <div class="todo__text">${input.value}</div>
-      <button class="button__delete">Удалить</button>
-      <input type="checkbox" value="checked"> 
-    </div>`;
-
     input.value = "";
+    return {
+      shouldRerender: true,
+    };
   }
 };
 
 const deleteToDo = (event) => {
-  event.target.parentElement.remove();
+  let numberToDo = Number(
+    event.target.parentElement.querySelector(".todo__text").textContent[0]
+  );
+
+  let filteeredTasks = tasks.filter((_, index) => index !== numberToDo);
+
+  tasks = filteeredTasks;
+  console.log(numberToDo);
+  console.log(filteeredTasks);
 };
 
-const deleteSpecificToDo = () => {
-  document.querySelectorAll(".todo__item").forEach((item) => {
-    if (item.children[2].checked == true) {
-      item.remove();
+
+const setEventListener = () => {
+  let allDeleteButton = document.querySelectorAll(".button__delete");
+  allDeleteButton.forEach((button) => {
+    button.addEventListener("click", deleteToDoAndRender);
+  });
+};
+
+const addToDoAndRerender = () => {
+  const { shouldRerender } = addToDo();
+
+  if (shouldRerender) {
+    renderToDoList();
+    setEventListener();
+  }
+};
+
+const deleteToDoAndRender = (event) => {
+  deleteToDo(event);
+  renderToDoList();
+  setEventListener();
+};
+
+const deleteAllToDoAndRender = () => {
+  deleteAllToDo();
+  renderToDoList();
+};
+
+const deleteSelectedToDosAndRender = () => {
+  deleteSelectedToDos();
+  setEventListener();
+  renderToDoList();
+};
+
+const getSelectedToDos = () => {
+  let allCheckBox = document.querySelectorAll(".input__checkbox");
+  allCheckBox.forEach((checkbox) => {
+    if (checkbox.checked) {
+      let idSelectedTask = checkbox.parentElement.getAttribute("data-id");
+      selectedTasks.push(idSelectedTask);
     }
   });
 };
 
+const deleteSelectedToDos = () => {
+  getSelectedToDos();
+
+  let filteredTasks = tasks.filter(
+    (item) => !selectedTasks.map((item) => Number(item)).includes(item.id)
+  );
+
+  tasks = filteredTasks;
+};
+
 const deleteAllToDo = () => {
-  todoList.innerHTML = "";
+  tasks = [];
 };
 
 input.addEventListener("input", getLastCharInInput);
 
 buttonInput.addEventListener("click", () => {
-  addToDo();
-  let todoItems = document.querySelectorAll(".button__delete");
-
-  todoItems.forEach((todoItem) => {
-    todoItem.addEventListener("click", deleteToDo);
-  });
+  addToDoAndRerender();
 });
 
-buttonDeleteSpecific.addEventListener("click", deleteSpecificToDo);
+buttonDeleteSpecific.addEventListener("click", deleteSelectedToDosAndRender);
 
-buttonDeleteAll.addEventListener("click", deleteAllToDo);
+buttonDeleteAll.addEventListener("click", deleteAllToDoAndRender);
